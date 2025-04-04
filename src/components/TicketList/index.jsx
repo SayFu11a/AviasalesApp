89174fae4f470bacec9c6e724e3e1623e8260ac3
Sayfu11a <sgishlakaev@gmail.com>
2025-04-04 09:11:01 +0300

    add loading

--- src/components/TicketList/index.jsx
+++ src/components/TicketList/index.jsx
@@ -27,23 +27,27 @@ function TicketList() {
 
     return (
         <>
-            {filteredTickeds.length > 0 ? (
-                <>
-                    {filteredTickeds?.slice(0, (page + 1) * 5).map((ticket) => (
-                        <Ticket key={ticket.segments[0].date} ticket={ticket} />
-                    ))}
-                    <Button type="primary" style={{ width: '100%' }} onClick={() => setPage((prev) => prev + 1)}>
-                        Показать еще 5 билетов!
-                    </Button>
-                </>
+            {!loading ? (
+                filteredTickeds.length > 0 ? (
+                    <>
+                        {filteredTickeds?.slice(0, (page + 1) * 5).map((ticket) => (
+                            <Ticket key={ticket.segments[0].date} ticket={ticket} />
+                        ))}
+                        <Button type="primary" style={{ width: '100%' }} onClick={() => setPage((prev) => prev + 1)}>
+                            Показать еще 5 билетов!
+                        </Button>
+                    </>
+                ) : (
+                    <Empty
+                        description={
+                            <Typography.Text>
+                                <span>Рейсов, подходящих под заданные фильтры, не найдено</span>
+                            </Typography.Text>
+                        }
+                    ></Empty>
+                )
             ) : (
-                <Empty
-                    description={
-                        <Typography.Text>
-                            <span>Рейсов, подходящих под заданные фильтры, не найдено</span>
-                        </Typography.Text>
-                    }
-                ></Empty>
+                <span>...загрузка</span>
             )}
         </>
     );
