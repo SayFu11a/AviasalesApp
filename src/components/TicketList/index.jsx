3ff1b1ceecfcece463f854066d9d62ffe71e6213
Sayfu11a <sgishlakaev@gmail.com>
2025-04-04 08:50:43 +0300

    add emty

--- src/components/TicketList/index.jsx
+++ src/components/TicketList/index.jsx
@@ -6,7 +6,7 @@ import { getFiltredTickets } from '../../Redux/selectors';
 import Ticket from '../Ticket';
 import { useEffect, useState } from 'react';
 
-import { Button } from 'antd';
+import { Button, Empty, Typography } from 'antd';
 
 function TicketList() {
     const { searchId, loading, error, tickets } = useSelector((state) => state.data);
@@ -27,12 +27,24 @@ function TicketList() {
 
     return (
         <>
-            {filteredTickeds?.slice(0, (page + 1) * 5).map((ticket) => (
-                <Ticket key={ticket.segments[0].date} ticket={ticket} />
-            ))}
-            <Button type="primary" style={{ width: '100%' }} onClick={() => setPage((prev) => prev + 1)}>
-                Показать еще 5 билетов!
-            </Button>
+            {filteredTickeds.length > 0 ? (
+                <>
+                    {filteredTickeds?.slice(0, (page + 1) * 5).map((ticket) => (
+                        <Ticket key={ticket.segments[0].date} ticket={ticket} />
+                    ))}
+                    <Button type="primary" style={{ width: '100%' }} onClick={() => setPage((prev) => prev + 1)}>
+                        Показать еще 5 билетов!
+                    </Button>
+                </>
+            ) : (
+                <Empty
+                    description={
+                        <Typography.Text>
+                            <span>Рейсов, подходящих под заданные фильтры, не найдено</span>
+                        </Typography.Text>
+                    }
+                ></Empty>
+            )}
         </>
     );
 }
